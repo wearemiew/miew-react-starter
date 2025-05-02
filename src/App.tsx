538ffa1934bd7432e039eedbf02591abf2afe191
Cristian Oliveira <cristian.oliveira@miew.pt>
2025-05-02 15:08:51 +0100

    Add SamplePage route and export in App and index files

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { Route, BrowserRouter, Routes } from "react-router";
 import "./App.css";
-import { HomePage } from "./pages";
+import { HomePage, SamplePage } from "./pages";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/sample" element={<SamplePage />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/Sample.page.tsx
@@ -0,0 +1,12 @@
+const SamplePage = () => {
+  return (
+    <div>
+      <header>
+        <h1>Miew React Starter Template</h1>
+        <p className="subtitle">sample route</p>
+      </header>
+    </div>
+  );
+};
+
+export default SamplePage;
